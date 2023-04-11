559cf0b70667ac53a32e4741074a4b94a5f4bb73
Vanja Mijatovic <vmijatovic2000@gmail.com>
2023-04-11 22:55:53 +0200

    Created Spring Boot project

---
+++ wavealerts/src/main/java/com/mijatovic/wavealerts/WaveAlertsApplication.java
@@ -0,0 +1,13 @@
+package com.mijatovic.wavealerts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaveAlertsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaveAlertsApplication.class, args);
+	}
+
+}
---
+++ wavealerts/src/test/java/com/mijatovic/wavealerts/WaveAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mijatovic.wavealerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaveAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
